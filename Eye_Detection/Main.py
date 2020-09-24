c2e9116d2e47d266c041ce95dce7aeccf3c50cae
Omerettrki <57484187+Omerettrki@users.noreply.github.com>
2020-09-24 21:39:47 +0300

    -

--- Eye_Detection/Main.py
+++ Eye_Detection/Main.py
@@ -24,35 +24,40 @@ def blinking_ratio(eye_points, face_landmarks):
 
     return ratio
 
-def eye_area (eye_points, face_landmarks):
 
+def gaze_direction(eye_points, face_landmarks):
     eye_region = np.array([(face_landmarks.part(eye_points[0]).x, face_landmarks.part(eye_points[0]).y),
-                                 (face_landmarks.part(eye_points[1]).x, face_landmarks.part(eye_points[1]).y),
-                                 (face_landmarks.part(eye_points[2]).x, face_landmarks.part(eye_points[2]).y),
-                                 (face_landmarks.part(eye_points[3]).x, face_landmarks.part(eye_points[3]).y),
-                                 (face_landmarks.part(eye_points[4]).x, face_landmarks.part(eye_points[4]).y),
-                                 (face_landmarks.part(eye_points[5]).x, face_landmarks.part(eye_points[5]).y)], np.int32)
+                           (face_landmarks.part(eye_points[1]).x, face_landmarks.part(eye_points[1]).y),
+                           (face_landmarks.part(eye_points[2]).x, face_landmarks.part(eye_points[2]).y),
+                           (face_landmarks.part(eye_points[3]).x, face_landmarks.part(eye_points[3]).y),
+                           (face_landmarks.part(eye_points[4]).x, face_landmarks.part(eye_points[4]).y),
+                           (face_landmarks.part(eye_points[5]).x, face_landmarks.part(eye_points[5]).y)], np.int32)
 
     height, width, _ = frame.shape
     mask = np.zeros((height, width), np.uint8)
     cv2.polylines(mask, [eye_region], True, 255, 2)
     cv2.fillPoly(mask, [eye_region], 255)
     eye = cv2.bitwise_and(gray, gray, mask=mask)
-    cv2.imshow("Binary Eye", eye)
 
     min_x = np.min(eye_region[:, 0])
     max_x = np.max(eye_region[:, 0])
     min_y = np.min(eye_region[:, 1])
     max_y = np.max(eye_region[:, 1])
 
-    gray_eye = eye[min_y:max_y , min_x:max_x]
-    _, thresholded_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
-
-    eye = cv2.resize(gray_eye, None, fx=5, fy=5)
+    gray_eye = eye[min_y:max_y, min_x:max_x]
+    _, thresholded_eye = cv2.threshold(gray_eye, 53, 255, cv2.THRESH_BINARY)
     thresholded_eye = cv2.resize(thresholded_eye, None, fx=5, fy=5)
-    cv2.imshow("Gray Eye", eye)
     cv2.imshow("Eye", thresholded_eye)
 
+    eye_height, eye_width= thresholded_eye.shape
+    left_side_eye = thresholded_eye[0:height, 0:int(width/2)]
+    right_side_eye = thresholded_eye[0:height, int(width/2):width]
+
+    size = np.size(thresholded_eye)
+    white = np.count_nonzero(thresholded_eye)
+
+    print("white : ", eye_height)
+    print("size : ", eye_width)
 
 
 COUNTER = 0
@@ -69,7 +74,7 @@ l_eye_region = [42, 43, 44, 45, 46, 47]
 
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(frame)
     for face in faces:
@@ -83,12 +88,12 @@ while True:
         l_blink_ratio = blinking_ratio(l_eye_region, landmarks)
         avg_blink_ratio = (l_blink_ratio + r_blink_ratio) / 2.0
 
-        eye_area(r_eye_region, landmarks)
+        gaze_direction(r_eye_region, landmarks)
 
-        if (avg_blink_ratio<0.2):
+        if (avg_blink_ratio < 0.2):
             COUNTER += 1
 
-        else :
+        else:
             if COUNTER >= 4:
                 TOTAL += 1
 
